7dd7db66c33442213288bece433c21f313217ef3
lamassu38 <116761877+lamassu38@users.noreply.github.com>
2023-10-01 19:17:57 -0500

    Update the route of the images in Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -38,7 +38,7 @@ export default function Main(){
             </div>
             <div className='image-container'>
                 {selectedCard && (<img 
-                    src={`/public/images/${selectedCard.card}`}
+                    src={`../images/${selectedCard.card}`}
                     alt={selectedCard.title}
                     className='lottery-image'
                 />)
