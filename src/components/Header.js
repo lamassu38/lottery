f5ed36f7c8987517a8958df163964cb73e85540e
Erika Contreras <ericontrerasleal2@gmail.com>
2023-10-02 22:28:09 -0500

    Change the image path and update the style.cc

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,6 @@ import React from 'react';
 export default function Header(){
     return (
         <header className='header'>
-            <img 
-            src=''
-            atl='Header logo'
-            className='header--image'
-            />
             <h2 className='header--title'>Mayan Lottery</h2>
             <h4 className='header--project'>React-app</h4>
         </header>
